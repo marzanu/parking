9a8a913332cf5d82c3cbf4e396b29b1239af3fde
marzanu <116082767+marzanu@users.noreply.github.com>
2022-10-20 09:36:52 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,27 +5,20 @@ input.onButtonPressed(Button.B, function () {
     plaçes_lliures = plaçes_lliures + 1
 })
 let plaçes_lliures = 0
-plaçes_lliures = 0
+plaçes_lliures = 5
 let clients = 0
 let aforament = 5
 basic.forever(function () {
     let plaçes_lliure = 0
-    if (plaçes_lliure == 5) {
-        basic.showNumber(5)
+    basic.showNumber(plaçes_lliures)
+    clients = aforament - plaçes_lliure
+    if (plaçes_lliures == 5 || plaçes_lliures > 5) {
+        plaçes_lliures = 5
     }
-    if (plaçes_lliure == 4) {
-        basic.showNumber(4)
+    if (plaçes_lliures == 0 || plaçes_lliures < 0) {
+        plaçes_lliures = 0
     }
-    if (plaçes_lliure == 3) {
-        basic.showNumber(3)
-    }
-    if (plaçes_lliure == 2) {
-        basic.showNumber(2)
-    }
-    if (plaçes_lliure == 1) {
-        basic.showNumber(1)
-    }
-    if (plaçes_lliure == 0) {
-        basic.showIcon(IconNames.No)
-    }
+})
+basic.forever(function () {
+	
 })
